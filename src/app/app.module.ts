de3fd0c9699fc38a75104cdf3f43531006932d34
Ganesan, Michael (LRC) <michael.ganesan@lrc.ky.gov>
2019-02-08 15:32:21 -0500

    added the news service component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import the material design components
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule
+} from '@angular/material';
+// Import News Service
+import { NewsApiService } from './service/news-api.service';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [NewsApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/service/news-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+  API_KEY = '4c3bd942b0b64ced96ac036d156c98a9';
+
+  constructor(private http: HttpClient) {}
+
+  initSources(): Observable<any> {
+
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.API_KEY);
+
+  }
+
+
+
+
+}
